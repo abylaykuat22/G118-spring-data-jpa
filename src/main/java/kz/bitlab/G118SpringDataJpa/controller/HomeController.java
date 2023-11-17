623b6dd32c15dea0df7bae1b80ba1bdcd8e46657
abylaykuat22 <abylaykuat@gmail.com>
2023-11-17 20:27:13 +0600

    @ManyToMany

--- src/main/java/kz/bitlab/G118SpringDataJpa/controller/HomeController.java
+++ src/main/java/kz/bitlab/G118SpringDataJpa/controller/HomeController.java
@@ -1,9 +1,13 @@
 package kz.bitlab.G118SpringDataJpa.controller;
 
 import java.util.List;
+import kz.bitlab.G118SpringDataJpa.model.Company;
 import kz.bitlab.G118SpringDataJpa.model.Developer;
+import kz.bitlab.G118SpringDataJpa.model.ProgrammingLanguage;
+import kz.bitlab.G118SpringDataJpa.repository.CompanyRepository;
 import kz.bitlab.G118SpringDataJpa.repository.DeveloperRepository;
 import kz.bitlab.G118SpringDataJpa.service.DeveloperService;
+import kz.bitlab.G118SpringDataJpa.service.ProgrammingLanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,22 +18,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+
   @Autowired
   private DeveloperRepository developerRepository;
   @Autowired
   private DeveloperService developerService;
+  @Autowired
+  private ProgrammingLanguageService programmingLanguageService;
+  @Autowired
+  private CompanyRepository companyRepository;
 
   @GetMapping("/")
   public String homePage(Model model) {
     List<Developer> developers = developerRepository.findAll();
+    List<ProgrammingLanguage> programmingLanguages = programmingLanguageService.getProgrammingLanguages();
     model.addAttribute("razraby", developers);
+    model.addAttribute("programmingLanguages", programmingLanguages);
     return "home";
   }
 
   @GetMapping("/developer/{id}")
   public String getDeveloper(@PathVariable Long id, Model model) {
     Developer developer = developerRepository.findById(id).orElse(null);
+    List<ProgrammingLanguage> programmingLanguages = programmingLanguageService.getProgrammingLanguages();
+    List<Company> companies = companyRepository.findAll();
+    if (developer != null) {
+      companies.removeAll(developer.getCompanies());
+    }
+    model.addAttribute("programmingLanguages", programmingLanguages);
     model.addAttribute("razrab", developer);
+    model.addAttribute("filteredCompanies", companies);
     return "developer-details";
   }
 
@@ -47,7 +65,6 @@ public class HomeController {
     return "home";
   }
 
-
   @PostMapping("/developer/add")
   public String addDeveloper(Developer developer) {
     developerRepository.save(developer);
@@ -58,5 +75,19 @@ public class HomeController {
   public String updateDeveloper(Developer developer) {
     developerRepository.save(developer);
     return "redirect:/";
+  }
+
+  @PostMapping("/developer/company/add")
+  public String addCompanyToDeveloper(@RequestParam Long developerId,
+      @RequestParam Long companyId) {
+    developerService.addCompany(developerId, companyId);
+    return "redirect:/developer/"+developerId;
+  }
+
+  @PostMapping("/developer/company/delete")
+  public String deleteCompanyFromDeveloper(@RequestParam Long developerId,
+      @RequestParam Long companyId) {
+    developerService.deleteCompany(developerId, companyId);
+    return "redirect:/developer/" + developerId;
   }
 }
--- src/main/java/kz/bitlab/G118SpringDataJpa/model/ProgrammingLanguage.java
+++ src/main/java/kz/bitlab/G118SpringDataJpa/model/ProgrammingLanguage.java
@@ -5,9 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.List;
@@ -18,34 +17,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "DEVELOPERS")
+@Table(name = "PROGRAMMING_LANGUAGES")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class Developer {
+public class ProgrammingLanguage {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "ID")
   private Long id;
 
-  @Column(name = "EMAIL", unique = true, nullable = false)
-  private String email;
+  @Column(name = "NAME", unique = true, nullable = false)
+  private String name;
 
-  @Column(name = "FULL_NAME", nullable = false)
-  private String fullName;
+  @Column(name = "DESCRIPTION", columnDefinition = "TEXT")
+  private String description;
 
-  @Column(name = "AGE")
-  private Integer age;
-
-  @ManyToOne
-  @JoinColumn(name = "PROGRAMMING_LANGUAGE_ID")
-  private ProgrammingLanguage programmingLanguage;
-
-  @OneToOne
-  private Position position;
-
-  @ManyToMany
-  private List<Company> companies;
 }
